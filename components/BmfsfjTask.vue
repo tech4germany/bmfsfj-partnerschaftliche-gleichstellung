d13232638bda611a83ef6d9b0a28bcc829f3c6eb
Malte Laukötter <malte@laukoetter.eu>
2021-10-12 16:52:29 +0200

    Change language selection to select

---
+++ components/BmfsfjTask.vue
@@ -0,0 +1,40 @@
+<template>
+  <bmfsfj-card>
+    <template #header> {{content != null ? content.title : ''}} </template>
+    <div>
+      <nuxt-content
+      class="mx-auto"
+      :document="content != null ? content.document : null" />
+
+    </div>
+  </bmfsfj-card>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRef } from '@nuxtjs/composition-api'
+import { useTask } from '~/utils/useTask';
+
+export default defineComponent({
+  props: {
+    done: {
+      type: Boolean,
+      default: false
+    },
+    taskName: {
+      default: 'mutterschaftsgeld-beantragen',
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    const task = useTask(toRef(props, 'taskName'))
+
+    return {
+      content: task
+    }
+  }
+})
+</script>
+
+<style>
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,46 +1,57 @@
 <template>
   <div>
-    <header class="">
-      <div class="w-full bg-primary-500 text-white h-16 flex flex-col justify-center">
-        <h1 class="ml-2"> {{$t('welcome')}} Team :)</h1>
+    <header>
+      <div class="w-full bg-primary-500 text-white h-16 flex flex-row justify-center">
+        <h1 class="ml-2 flex-grow"> {{$t('welcome')}} Team :)</h1>
+
+
+        <select class="bg-primary-300 p-2" :value="currentLocale" @input="switchLocale">
+          <option
+            v-for="locale in availableLocales"
+            :key="locale.code"
+            class="hover:bg-yellow-500"
+            :value="locale.code"
+          >{{ locale.name }}</option>
+        </select>
       </div>
 
       <nav class="flex w-full text-center text-primary-500 font-bold">
         <NuxtLink :to="localeRoute('/tasks')" class="flex-grow m-2">Aufgaben</NuxtLink>
-        <NuxtLink :to="localeRoute('/')" class="flex-grow m-2">Übersicht</NuxtLink>
+        <NuxtLink :to="localeRoute('/test')" class="flex-grow m-2">Übersicht</NuxtLink>
         <NuxtLink :to="localeRoute('/intro/married')" class="flex-grow m-2">Wir</NuxtLink>
       </nav>
-
-      <div>
-        <nuxt-link
-          v-for="locale in availableLocales"
-          :key="locale.code"
-          :to="switchLocalePath(locale.code)">{{ locale.name }}</nuxt-link>
-      </div>
     </header>
 
-    <Nuxt />
+    <div class="m-2">
+      <Nuxt />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, wrapProperty } from '@nuxtjs/composition-api'
+import { computed, defineComponent, useRouter, wrapProperty } from '@nuxtjs/composition-api'
 import type { LocaleObject } from '@nuxtjs/i18n';
 
 export const useI18n = wrapProperty('$i18n', false)
+export const useSwitchLocalePath = wrapProperty('switchLocalePath', false)
 
 export default defineComponent({
   setup(_props) {
     const $i18n = useI18n();
+    const $router = useRouter();
+    const switchLocalePath = useSwitchLocalePath();
+
     return {
-      availableLocales: computed(() => ($i18n.locales as LocaleObject[]).filter(i => i.code !== $i18n.locale))
+      availableLocales: computed(() => ($i18n.locales as LocaleObject[])),
+      currentLocale: computed(() => $i18n.locale),
+      switchLocale: (e: InputEvent) => $router.push(switchLocalePath((e.target! as HTMLSelectElement).value))
     }
   },
 })
 </script>
 
 <style>
-  .nuxt-link-exact-active {
+  .nuxt-link-active {
     @apply border-b-2 border-primary-500;
   }
 </style>
