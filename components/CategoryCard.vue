13b6c4b59bde4ba629168b24f6c265ed11951a7e
Malte Laukötter <malte@laukoetter.eu>
2021-10-15 15:38:49 +0200

    Add simple module cards

--- components/CategoryCard.vue
+++ components/CategoryCard.vue
@@ -1,30 +1,67 @@
 <template>
-  <div>
-    <div @click="increment">{{name}} - {{counter}}</div>
+  <div class="category-card" :style="`--color: ${color};`">
+    <div class="growing-background-parent">
+      <div
+        class="growing-background"
+        :style="`--background-width: ${percentDone}%;`"
+      >
+        <span class="flex-grow m-2">{{ name }}</span>
+        <span class="m-2">{{ percentDone }}%</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from '@nuxtjs/composition-api'
+import { defineComponent } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   props: {
     name: {
-      default: "test",
-      type: String
-    }
+      default: 'test',
+      type: String,
+    },
+    percentDone: {
+      default: 0,
+      type: Number,
+    },
+    color: {
+      default: '#00797f',
+      type: String,
+    },
   },
-  setup(_props) {
-    const counter = ref(0);
-
-    const increment = () => {
-      counter.value++;
-    }
-
-    return {
-      counter,
-      increment
-    }
-  }
 })
 </script>
+
+<style scoped>
+.category-card {
+  @apply rounded-xl font-bold border-4 relative;
+  border-color: var(--color);
+}
+
+.growing-background-parent {
+  filter: invert();
+}
+
+.growing-background {
+  @apply flex;
+  width: 100%;
+}
+
+.growing-background::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: var(--background-width);
+  height: 100%;
+  filter: invert();
+  background-color: var(--color);
+}
+
+.growing-background span {
+  filter: invert();
+  color: var(--color);
+  mix-blend-mode: difference;
+}
+</style>
--- pages/modules.vue
+++ pages/modules.vue
@@ -1,3 +1,8 @@
 <template>
-  <div>Module</div>
+  <div class="flex flex-col gap-2">
+    <category-card color="#d3003a" name="Geld" :percent-done="50"></category-card>
+    <category-card color="#e04a09" name="Beruf" :percent-done="70"></category-card>
+    <category-card color="#004377" name="Behörden & Papiere" :percent-done="30"></category-card>
+    <category-card color="#008136" name="Gesundheit" :percent-done="97"></category-card>
+  </div>
 </template>
