6fcaee4d8c6d203bd2f143149bc33af810736e13
Malte Laukötter <malte@laukoetter.eu>
2021-10-18 09:02:28 +0200

    Setup vs code debugger

--- nuxt.config.js
+++ nuxt.config.js
@@ -107,6 +107,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extend(config, ctx) {
+      if (ctx.isDev) {
+        config.devtool = ctx.isClient ? 'source-map' : 'inline-source-map'
+      }
+    },
   },
 
   purgeCSS: {
