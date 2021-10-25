5c196f4f786af95c40fd20e0747d23898a86e7e9
Malte Laukötter <malte@laukoetter.eu>
2021-10-25 12:41:15 +0200

    Update html title

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'bmfsfj-partnerschaftliche-gleichstellung',
+    title: 'StartApp Familie',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
